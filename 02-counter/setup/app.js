608ac785fbf34366588000131b9c71bbad7c0492
mcasasExxon <manuel.m.casas@exxonmobil.com>
2023-03-08 16:33:57 -0300

    Counter project done

--- 02-counter/setup/app.js
+++ 02-counter/setup/app.js
@@ -1,3 +1,5 @@
+/*
+
 const increment = document.getElementById("btn-inc");
 const decrement = document.getElementById("btn-dec");
 const restart = document.getElementById("btn-rst");
@@ -19,4 +21,36 @@ decrement.addEventListener("click", function () {
 restart.addEventListener("click", function () {
   //value = 0;
   value.textContent = 0;
+});
+
+*/
+
+let count = 0;
+
+//select value and buttons
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increment")) {
+      count++;
+    } else if (styles.contains("reset")) {
+      count = 0;
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
